49873b32be01006aa66e72e2364d34f0b845e33e
Руслан Гумеров <ruslan0gumerov@gmail.com>
2019-09-18 00:14:00 +0300

    Version 17.09.19

--- longevity.R
+++ longevity.R
@@ -5,7 +5,7 @@ library(org.Mm.eg.db)
 current_gse = getGEO("gse55272")
 #create phenodata table
 current_gse_phenodata = data.frame(pData(phenoData(current_gse[[1]]))[,c(44,41,40,42,43,25)])
-current_gse_phenodata = subset(gse55272_phd, tissue.ch1=="Liver")
+current_gse_phenodata = subset(current_gse_phenodata, tissue.ch1=="Liver")
 current_gse_phenodata = current_gse_phenodata %>% mutate("diet" = "-", "drug" = "-", "dose" = "-","age_intervention" = "-")
 #create assayData table
 current_gse_assayData = assayData((current_gse[[1]]))[["exprs"]]
