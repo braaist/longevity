7dc9de6f46a04328518c717cc6decbdebaa55dc1
Руслан Гумеров <braaist@MacBook-Air-Ruslan.local>
2020-02-02 15:35:59 +0300

    gse71255 and gse81891

--- functions.R
+++ functions.R
@@ -1,3 +1,12 @@
+#FUNCTIONS USED IN THIS PROJECT
+#get GSE file create phenodata table
+library(GEOquery)
+library(dplyr)
+library(ggplot2)
+library(limma)
+library(biomaRt)
+library(org.Mm.eg.db)
+
 entrez_converter <- function(ACCNUM){
   #function for convertation ACCNUM to ENTREZID
   if(is.null(xx[[as.character(ACCNUM)]])){
@@ -11,7 +20,64 @@ limma_maker <- function(assayData, des_matrix, cont_matrix){
   fit = lmFit(assayData, des_matrix)
   fit_contrast = eBayes(contrasts.fit(fit, cont_matrix))
   volcanoplot(fit_contrast)
-  top_genes = topTable(fit_contrast, number = Inf, adjust = "BH")
+  top_genes = topTable(fit_contrast, number = Inf, adjust = "BH", confint = TRUE)
+  top_genes$SE = (top_genes$CI.R-top_genes$CI.L)/3.92
   top_genes_limma <<- top_genes
   return(top_genes)
 }
+
+ensembl2entrez_mm <- function(ENSEMBL_IDS){
+  ENTREZ_IDS = c()
+  x <- org.Mm.egENSEMBL2EG
+  mapped_probes = mappedkeys(x)
+  xx = as.list(x[mapped_probes])
+  for (i in ENSEMBL_IDS){
+  if(is.null(xx[[as.character(i)]])){
+    ENTREZ_IDS = c(ENTREZ_IDS, NA)
+    }
+  else if(length(xx[[as.character(i)]]) != 1){
+    ENTREZ_IDS = c(ENTREZ_IDS, NA)
+  }else{
+    ENTREZ_IDS = c(ENTREZ_IDS, xx[[as.character(i)]])
+    }
+  }
+  return(ENTREZ_IDS)
+}
+affy2entrez_mm <- function(AFFY_IDS){
+  ENTREZ_IDS = c()
+  x <- mouse430a2ENTREZID
+  mapped_probes = mappedkeys(x)
+  xx = as.list(x[mapped_probes])
+  for (i in AFFY_IDS){
+    if(is.null(xx[[as.character(i)]])){
+      ENTREZ_IDS = c(ENTREZ_IDS, NA)
+    }
+    else if(length(xx[[as.character(i)]]) != 1){
+      ENTREZ_IDS = c(ENTREZ_IDS, NA)
+    }else{
+      ENTREZ_IDS = c(ENTREZ_IDS, xx[[as.character(i)]])
+    }
+  }
+  return(ENTREZ_IDS)
+}
+
+ilmna2entrez_hs <- function(ILMN_IDS){
+  ENTREZ_IDS = c()
+  x <- illuminaHumanv4ENTREZID
+  mapped_probes = mappedkeys(x)
+  xx = as.list(x[mapped_probes])
+  for (i in ILMN_IDS){
+    if(is.null(xx[[as.character(i)]])){
+      ENTREZ_IDS = c(ENTREZ_IDS, NA)
+    }
+    else if(length(xx[[as.character(i)]]) != 1){
+      ENTREZ_IDS = c(ENTREZ_IDS, NA)
+    }else{
+      ENTREZ_IDS = c(ENTREZ_IDS, xx[[as.character(i)]])
+    }
+  }
+  return(ENTREZ_IDS)
+}
+
+
+
---
+++ gse81891/gse81891.R
@@ -0,0 +1,306 @@
+library(GEOquery)
+library(dplyr)
+library(ggplot2)
+library(limma)
+source("functions.R")
+library(biomaRt)
+library(preprocessCore)
+library(biomaRt)
+library(illuminaHumanv4.db)
+
+####################################################################################
+####################################################################################
+#PLATFORM:  Illumina HumanHT-12 V4.0 expression beadchip
+#2 types of factors: OSKM and OSvKM
+####################################################################################
+####################################################################################
+current_gse = "gse81891"
+current_gse_geo = getGEO(current_gse)
+#STEP 0: create phenodata table
+current_gse_phenoData = pData(current_gse_geo[[1]])
+current_gse_assayData = exprs(current_gse_geo[[1]])
+current_gse_featureData = fData(current_gse_geo[[1]])
+#and check density
+current_gse_assayData_fd = data.frame(pops)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density before all", current_gse, sep = " "))
+
+
+
+################################################################################################
+################################################################################################
+#OS(v)KM
+################################################################################################
+################################################################################################
+
+
+#STEP 1: logtransformation (data is not transformed)
+#seems already transformed, it's actually strange
+#current_gse_assayData = log(current_gse_assayData + 1)
+
+#STEP 2: subset data
+current_gse_phenoData = current_gse_phenoData[7:11,]
+current_gse_assayData = current_gse_assayData[,7:11]
+current_gse_featureData = current_gse_featureData
+rownames(current_gse_assayData) = current_gse_featureData$ID
+#STEP 3: remove outliers
+#current_gse_assayData = current_gse_assayData[,c(1:2, 4:10)]
+#current_gse_phenoData = current_gse_phenoData[c(1:2, 4:10),]
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density before OS(v)KM", current_gse, sep = " "))
+
+
+
+#STEP 4: filter by treshold
+#looks quite good, no filtering
+#filter_table = current_gse_assayData_MEF >= -5
+#filter_table = data.frame(filter_table)
+#filter_table = filter_table %>% mutate(sum = rowSums(.[1:ncol(filter_table)])) %>% mutate(filter = sum>2)
+#filter_table = data.frame(filter_table)
+#filter_treshold = filter_table$filter
+#current_gse_assayData_MEF = current_gse_assayData_MEF[filter_treshold,]
+#current_gse_featureData_MEF = current_gse_featureData_MEF[filter_treshold,]
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density after filtering by treshold OS(v)KM", current_gse, sep = " "))
+
+
+#STEP 5: scaling and quantile normalization
+#scaling
+current_gse_assayData = scale(current_gse_assayData)
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density after scaling OS(v)KM", current_gse, sep = " "))
+
+#quantile normalization
+current_gse_assayData = normalize.quantiles(as.matrix(current_gse_assayData))
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density after quantile normalization OS(v)KM", current_gse, sep = " "))
+colnames(current_gse_assayData) = rownames(current_gse_phenoData)
+rownames(current_gse_assayData) = current_gse_featureData$ID
+
+#STEP 6: add entrez ids
+#add entrez ids
+entrez_vec = ilmna2entrez_hs(current_gse_featureData$ID)
+current_gse_featureData$entrez = entrez_vec
+
+#delete genes without ENTREZID
+NAfilter = !is.na(current_gse_featureData$entrez)
+current_gse_assayData = current_gse_assayData[NAfilter,]
+current_gse_featureData = current_gse_featureData[NAfilter,]
+#STEP 7: replace genes with the same entrez id 
+#get lists of genes with the same ENTREZID
+same_entrez_list = list()
+'%notin%' = Negate('%in%')
+for (i in rownames(current_gse_featureData)){
+  print(i)
+  if ((length(which(current_gse_featureData[i,'entrez'] == current_gse_featureData[,'entrez'])) != 1) && (list(which(current_gse_featureData[i,'entrez'] == current_gse_featureData[,'entrez'])) %notin% same_entrez_list)){
+    same_entrez_list = append(same_entrez_list, list(which(current_gse_featureData[i,'entrez'] == current_gse_featureData[,'entrez'])))
+  }
+}
+#get mean of the microarray data for same ENTREZID
+for (i in same_entrez_list){
+  same_entrez_matrix = double()
+  for (j in i){
+    same_entrez_matrix = rbind(same_entrez_matrix, as.double(current_gse_assayData[j,]))}
+  mean_of_samples = apply(same_entrez_matrix, 2, mean)
+  current_gse_assayData[i[1],] = mean_of_samples
+  i = i[-1]
+  for (m in i){
+    current_gse_assayData[m,] = NA
+    current_gse_featureData[m,] = NA}
+}
+
+current_gse_assayData = na.omit(current_gse_assayData)
+current_gse_featureData = current_gse_featureData[!is.na(current_gse_featureData$entrez), ]
+current_gse_assayData = data.frame(current_gse_assayData)
+rownames(current_gse_assayData) = current_gse_featureData$entrez
+
+#STEP 8: PCA plot
+gse_for_cluster = scale(t(current_gse_assayData))
+#check scaling
+sd(gse_for_cluster[1,])
+#make PCA plot
+pcamodel = prcomp(gse_for_cluster)
+cluster_values = as.data.frame(pcamodel$x)
+cluster_plot = ggplot(cluster_values, aes(cluster_values[,1], cluster_values[,2])) +
+  geom_point(aes(color = current_gse_phenoData$title)) + 
+  labs(colour = "days", title = paste("PCA plot","OS(v)KM", current_gse, sep = " "))
+cluster_plot
+
+#STEP 8: final scaling
+#scaling
+current_gse_assayData = scale(current_gse_assayData)
+#and check density again
+current_gse_assayData_fd = current_gse_assayData
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("final density OS(v)KM", current_gse, sep = " "))
+
+
+#STEP 9: limma
+days_vec = as.numeric(c(rep(1,3), rep(0,2)))
+design_matrix = cbind(c(rep(1, length(days_vec))), days_vec)
+colnames(design_matrix) <- c("Intercept", "Age")
+rownames(design_matrix) = colnames(current_gse_assayData)
+contrast_matrix <- makeContrasts("Age", levels=design_matrix)
+top_genes_limma = limma_maker(current_gse_assayData, design_matrix, contrast_matrix)
+
+first_entity = rownames(top_genes_limma[1,])
+first_entity_df = current_gse_assayData[first_entity,]
+first_entity_df = data.frame(first_entity_df)
+first_entity_df$day = days_vec
+ggplot(first_entity_df, aes(day, first_entity_df, color = day))  + geom_point() +
+  labs(colour = "days", x = "age", y = "expression", title = paste("entrez ID MEF", first_entity, current_gse, sep = " ")) +
+  scale_x_continuous(labels = as.character(days_vec), breaks = days_vec)
+
+save(top_genes_limma,file="gse81891/top_genes_OS(v)KM.Rda")
+
+################################################################################################
+################################################################################################
+#OSKM
+################################################################################################
+################################################################################################
+
+
+#STEP 2: subset data
+#OSKM
+current_gse_phenoData = current_gse_phenoData[c(4:6, 10:11),]
+current_gse_assayData = current_gse_assayData[,c(4:6, 10:11)]
+current_gse_featureData = current_gse_featureData
+rownames(current_gse_assayData) = current_gse_featureData$ID
+#STEP 3: remove outliers
+#current_gse_assayData = current_gse_assayData[,c(1:2, 4:10)]
+#current_gse_phenoData = current_gse_phenoData[c(1:2, 4:10),]
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density before OSKM", current_gse, sep = " "))
+
+
+
+#STEP 4: filter by treshold
+#looks quite good, no filtering
+#filter_table = current_gse_assayData_MEF >= -5
+#filter_table = data.frame(filter_table)
+#filter_table = filter_table %>% mutate(sum = rowSums(.[1:ncol(filter_table)])) %>% mutate(filter = sum>2)
+#filter_table = data.frame(filter_table)
+#filter_treshold = filter_table$filter
+#current_gse_assayData_MEF = current_gse_assayData_MEF[filter_treshold,]
+#current_gse_featureData_MEF = current_gse_featureData_MEF[filter_treshold,]
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density after filtering by treshold MEF", current_gse, sep = " "))
+
+
+#STEP 5: scaling and quantile normalization
+#scaling
+current_gse_assayData = scale(current_gse_assayData)
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density after scaling OSKM", current_gse, sep = " "))
+
+#quantile normalization
+current_gse_assayData = normalize.quantiles(as.matrix(current_gse_assayData))
+#and check density again
+current_gse_assayData_fd = data.frame(current_gse_assayData)
+visualstack = stack(current_gse_assayData_fd)
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("density after quantile normalization MEF", current_gse, sep = " "))
+colnames(current_gse_assayData) = rownames(current_gse_phenoData)
+rownames(current_gse_assayData) = current_gse_featureData$ID
+
+#STEP 6: add entrez ids
+#add entrez ids
+entrez_vec = ilmna2entrez_hs(current_gse_featureData$ID)
+current_gse_featureData$entrez = entrez_vec
+
+#delete genes without ENTREZID
+NAfilter = !is.na(current_gse_featureData$entrez)
+current_gse_assayData = current_gse_assayData[NAfilter,]
+current_gse_featureData = current_gse_featureData[NAfilter,]
+#STEP 7: replace genes with the same entrez id 
+#get lists of genes with the same ENTREZID
+same_entrez_list = list()
+'%notin%' = Negate('%in%')
+for (i in rownames(current_gse_featureData)){
+  print(i)
+  if ((length(which(current_gse_featureData[i,'entrez'] == current_gse_featureData[,'entrez'])) != 1) && (list(which(current_gse_featureData[i,'entrez'] == current_gse_featureData[,'entrez'])) %notin% same_entrez_list)){
+    same_entrez_list = append(same_entrez_list, list(which(current_gse_featureData[i,'entrez'] == current_gse_featureData[,'entrez'])))
+  }
+}
+#get mean of the microarray data for same ENTREZID
+for (i in same_entrez_list){
+  same_entrez_matrix = double()
+  for (j in i){
+    same_entrez_matrix = rbind(same_entrez_matrix, as.double(current_gse_assayData[j,]))}
+  mean_of_samples = apply(same_entrez_matrix, 2, mean)
+  current_gse_assayData[i[1],] = mean_of_samples
+  i = i[-1]
+  for (m in i){
+    current_gse_assayData[m,] = NA
+    current_gse_featureData[m,] = NA}
+}
+
+current_gse_assayData = na.omit(current_gse_assayData)
+current_gse_featureData = current_gse_featureData[!is.na(current_gse_featureData$entrez), ]
+current_gse_assayData = data.frame(current_gse_assayData)
+rownames(current_gse_assayData) = current_gse_featureData$entrez
+
+#STEP 8: PCA plot
+gse_for_cluster = scale(t(current_gse_assayData))
+#check scaling
+sd(gse_for_cluster[1,])
+#make PCA plot
+pcamodel = prcomp(gse_for_cluster)
+cluster_values = as.data.frame(pcamodel$x)
+cluster_plot = ggplot(cluster_values, aes(cluster_values[,1], cluster_values[,2])) +
+  geom_point(aes(color = current_gse_phenoData$title)) + 
+  labs(colour = "days", title = paste("PCA plot","OSKM", current_gse, sep = " "))
+cluster_plot
+
+#STEP 8: final scaling
+#scaling
+current_gse_assayData = scale(current_gse_assayData)
+#and check density again
+current_gse_assayData_fd = current_gse_assayData
+ggplot(visualstack, aes=(x=values)) + geom_density(aes(x=values, group=ind, color=ind)) + 
+  labs(colour = "days", title = paste("final density OSKM", current_gse, sep = " "))
+
+
+#STEP 9: limma
+days_vec = as.numeric(c(rep(1,3), rep(0,2)))
+design_matrix = cbind(c(rep(1, length(days_vec))), days_vec)
+colnames(design_matrix) <- c("Intercept", "Age")
+rownames(design_matrix) = colnames(current_gse_assayData)
+contrast_matrix <- makeContrasts("Age", levels=design_matrix)
+top_genes_limma = limma_maker(current_gse_assayData, design_matrix, contrast_matrix)
+
+first_entity = rownames(top_genes_limma[1,])
+first_entity_df = current_gse_assayData[first_entity,]
+first_entity_df = data.frame(first_entity_df)
+first_entity_df$day = days_vec
+ggplot(first_entity_df, aes(day, first_entity_df, color = day))  + geom_point() +
+  labs(colour = "days", x = "age", y = "expression", title = paste("entrez ID OSKM", first_entity, current_gse, sep = " ")) +
+  scale_x_continuous(labels = as.character(days_vec), breaks = days_vec)
+
+save(top_genes_limma,file="gse81891/top_genes_OSKM.Rda")
+
+
+
+
