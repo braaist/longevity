bd7d1e1215c0c459895d1a2c4e31006520db0530
Руслан Гумеров <braaist@MacBook-Air-Ruslan.local>
2020-02-21 16:51:28 +0300

    deming script for globe server

---
+++ deming_script.R
@@ -0,0 +1,41 @@
+logFCmatrixregr = readRDS("logFCmatrixregr.Rds")
+cortestsign500 = readRDS("cortestsign500.Rds")
+totalrownamematrix = readRDS("totalrownamematrix.Rds")
+deming_minimizer = function(logFCmatrixregr){
+  fn = function(k_no_first){
+    k = c()
+    k[1] = 1
+    k[2:length(colnames(logFCmatrixregr))] = k_no_first
+    res = 0
+    for (i in 1:(length(colnames(logFCmatrixregr))-1)){
+      namei = colnames(logFCmatrixregr)[i]
+      for (j in (i + 1):length(colnames(logFCmatrixregr))){
+        namej = colnames(logFCmatrixregr)[j]
+        if (cortestsign500[namei, namej] != 1){
+          next
+        }
+        totalrownames = totalrownamematrix[[namei]][[namej]]
+        ai = logFCmatrixregr[totalrownames, namei]
+        aj = logFCmatrixregr[totalrownames, namej]
+        res = res + sum(
+          (((aj - (k[j]/k[i])*ai)^2)*((ai - (k[i]/k[j])*aj)^2))/
+            (((aj - (k[j]/k[i])*ai)^2)+((ai - (k[i]/k[j])*aj)^2)))/length(totalrownames)
+      }
+    }
+    return(res)
+  }
+  
+  kvec = rnorm(length(colnames(logFCmatrixregr)) - 1, 1, 1)
+  ptm <- proc.time()
+  optimized = optim(kvec, fn, lower = 0.01, upper = 100, method = "L-BFGS-B")
+  proc.time() - ptm
+  kres = c(1, optimized$par)
+  minimum = optimized$value
+  bigres = list(kres, minimum)
+  names(bigres) = c("coefs", "minimum")
+  return(bigres)
+}
+
+
+res = deming_minimizer(logFCmatrixregr)
+saveRDS(res, file = "deming_result.Rds")
